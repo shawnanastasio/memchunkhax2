b796b98c80a2aa093e88565c1352b37a4fde9a8a
Shawn Anastasio <shawnanastasio@yahoo.com>
2016-01-07 23:03:28 -0600

    Modularize service name in demo (for debugging)

--- source/main.c
+++ source/main.c
@@ -4,17 +4,28 @@
 
 #include "memchunkhax2.h"
 
+void testService(const char *name) {
+  Handle amHandle = 0;
+  Result res;
+
+  // Call am:u service
+  res = srvGetServiceHandleDirect(&amHandle, name);
+  printf("%s init1 result/handle: res=%lu handle=%lu\n", name, res, amHandle);
+  if(amHandle) {
+      svcCloseHandle(amHandle);
+  }
+}
+
 int main(int argc, char **argv) {
     //mode
     //1 = only execute memchunkhax2
     //2 = only execute service call
     //3 = execute memchunkhax2 and service call afterwards
     //4 = execute all
-    u8 mode = 3;
-
-    Handle amHandle = 0;
-    Result res;
+    u8 mode = 2;
     u8 success;
+
+    const char* service = "am:u";
 
     gfxInitDefault();
     consoleInit(GFX_TOP, NULL);
@@ -26,12 +37,7 @@ int main(int argc, char **argv) {
     }
 
     if (mode == 2) {
-      // Call am:u service
-      res = srvGetServiceHandleDirect(&amHandle, "am:u");
-      printf("am:u init1 result/handle: res=%lu handle=%lu\n", res, amHandle);
-      if(amHandle) {
-          svcCloseHandle(amHandle);
-      }
+      testService(service);
     }
 
     if (mode == 3) {
@@ -39,45 +45,33 @@ int main(int argc, char **argv) {
       success = execute_memchunkhax2();
       printf("Exploit returned: %s\n", success ? "Success!" : "Failure.");
 
-      // Call am:u service
-      res = srvGetServiceHandleDirect(&amHandle, "am:u");
-      printf("am:u init2 result/handle: res=%lu handle=%lu\n", res, amHandle);
-      if(amHandle) {
-          svcCloseHandle(amHandle);
-      }
+      testService(service);
     }
 
     if (mode == 4) {
       // This one should fail
-      res = srvGetServiceHandleDirect(&amHandle, "am:u");
-      printf("am:u init1 result/handle: res=%lu handle=%lu\n", res, amHandle);
-      if(amHandle) {
-          svcCloseHandle(amHandle);
-      }
+      testService(service);
 
       // Run the exploit
       success = execute_memchunkhax2();
       printf("Exploit returned: %s\n", success ? "Success!" : "Failure.");
 
       // This one hopefully won't
-      res = srvGetServiceHandleDirect(&amHandle, "am:u");
-      printf("am:u init2 result/handle: res=%lu handle=%lu\n", res, amHandle);
-      if(amHandle) {
-          svcCloseHandle(amHandle);
-      }
+      testService(service);
     }
 
     printf("Press START to exit.\n");
 
     while(aptMainLoop()) {
-        hidScanInput();
-        if(hidKeysDown() & KEY_START) {
-            break;
-        }
 
-        gfxFlushBuffers();
-        gfxSwapBuffers();
-        gspWaitForVBlank();
+      hidScanInput();
+      if(hidKeysDown() & KEY_START) {
+          break;
+      }
+
+      gfxFlushBuffers();
+      gfxSwapBuffers();
+      gspWaitForVBlank();
     }
 
     gfxExit();
